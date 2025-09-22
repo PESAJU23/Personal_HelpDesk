f0acd503ec117eff83648493099873c0ef372fb4
Pedro juan sarmiento julio <pesaju.2001@gmail.com>
2025-09-22 11:00:43 -0500

    Personal_HelpDesk

---
+++ config/Conexion.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+
+    class Conectar {
+        protected $dbh;
+
+        protected function Conexion() {
+            try {
+                $conectar = $this->dbh = new PDO("mysql:host=localhost;dbname=globalcruises_helpdesk", "root", "2001423");
+                return $conectar;
+            } catch (Exception $e) {
+                print "Error BD!: " . $e->getMessage() . "<br/>";
+                die();
+            }
+        }
+
+        public function set_names() {
+            return $this->dbh->query("SET NAMES 'utf8'");
+        }
+
+        public function ruta() {
+            return "http://localhost:80/PERSONAL_HelpDesk/";
+        }
+    }
+?>
